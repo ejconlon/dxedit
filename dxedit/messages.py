90bd933582f99ef563197b68db53f830fee9282f
Eric Conlon <ejconlon@gmail.com>
2013-12-25 13:25:59 -0500

    spec code

--- dxedit/messages.py
+++ dxedit/messages.py
@@ -2,7 +2,11 @@ from .enums import *
 from .matchers import *
 from .constants import *
 
-messages = {
+# matcher(byte): byte or None
+# message_specs: { message_type : [(B, N, matcher(byte))] }
+# Each spec is a list of sections that can have at most one "N.many" section.
+# (This restriction eliminates the need for backtracking.)
+message_specs = {
     'dx_param_change': [
         (B.sysex_start, N.one, match_equals(start_tag)),
         (B.mfr_id, N.one, match_equals(yamaha_mfr_id)),
@@ -28,3 +32,60 @@ messages = {
         (B.sysex_end, N.one, match_equals(end_tag))
     ]
 }
+
+# seq: [byte]
+# returns: (message_type, [(B, [byte])]) or None
+def parse_seq(seq):
+    for (message_type, spec) in message_specs:
+        parsed = parse_seq_with_spec(seq, spec)
+        if parsed is not None:
+            return (message_type, parsed)
+    return None
+
+def num_many(spec):
+    m = 0
+    for section in spec:
+        if section[1] == N.many:
+            m += 1
+    return m
+
+def validate_specs(specs):
+    for spec in specs.values():
+        m = num_many(spec)
+        if m > 1:
+            raise Exception("Specs may only have one many section")
+
+validate_specs(message_specs)
+
+def parse_seq_with_spec(seq, spec):
+    m = num_many(spec)
+    # Don't bother matching sequences that are too short to satisfy
+    if len(seq) < len(spec) - m:
+        return None
+    rs = []
+    seq_i = 0
+    spec_i = 0
+    while spec_i < len(spec):
+        section = spec[spec_i]
+        if section[1] == N.many:
+            # start from back now, then match the many
+            break
+        r = match(seq[seq_i], section)
+        if r is None:
+            return None
+        else:
+            rs.append(r)
+        seq_i += 1
+        spec_i += 1
+    # if we broke early for a many section,
+    if spec_i < len(spec):
+        raise Exception("TODO")
+
+def match(byte, section):
+    assert section[1] == N.one
+    r = section[2](byte)
+    if r is None:
+        return None
+    else:
+        return (section[0], r)
+
