59a89c627479011381f6453bb82983d30249bc15
Eric Conlon <ejconlon@gmail.com>
2013-12-25 12:13:53 -0500

    adding test

--- dxedit/matchers.py
+++ dxedit/matchers.py
@@ -5,17 +5,19 @@ def match_equals(x):
             return [x]
         else:
             return None
-    f
+    return f
 
 def match_any():
     def f(y):
         return [y]
+    return f
 
 def match_like(p):
     if len(p) != 8:
         raise Exception("Need 8bit pattern")
     def f(y):
         return [y] # TODO
+    return f
 
 def match_seven():
     return match_like('0vvvvvvv')
