5bbafd53b1b0ce0cb77579f11f80dd1df5ad018f
Eric Conlon <ejconlon@gmail.com>
2013-12-29 18:58:24 -0500

    all message lookup tables

--- DXEdit.scala
+++ DXEdit.scala
@@ -98,7 +98,7 @@ object DXEdit {
   import TagType._
   import TableType._
 
-  case class LookupTable[N, Row](name: N, size: Int, rows: Seq[Row])
+  case class LookupTable[N, Row](name: N, sizeForCheck: Int, rows: Seq[Row])
 
   type ByteMatcher = Byte => Try[Byte]
   type FrameMatcher = Frame => Try[PSeq]
@@ -132,6 +132,46 @@ object DXEdit {
           (ADDR_LOW, ONCE, matchSeven),
           (DATA, MANY, matchSeven),
           (CHECKSUM, ONCE, matchSeven),
+          (SYSEX_END, ONCE, matchEquals(END_TAG))
+        )
+      ),
+      FrameTable(
+        DX_BULK_DUMP, 9,
+        Seq(
+          (SYSEX_START, ONCE, matchEquals(START_TAG)),
+          (MFR_ID, ONCE, matchEquals(YAMAHA_MFR_ID)),
+          (DEVICE_NUM, ONCE, matchLike("0000nnnn")),
+          (FORMAT_NUM, ONCE, matchSeven),
+          (BYTE_COUNT_MSB, ONCE, matchSeven),
+          (BYTE_COUNT_LSB, ONCE, matchSeven),
+          (DATA, MANY, matchSeven),
+          (CHECKSUM, ONCE, matchSeven),
+          (SYSEX_END, ONCE, matchEquals(END_TAG))
+        )
+      ),
+      FrameTable(
+        DX200_NATIVE_PARAM_CHANGE, 9,
+        Seq(
+          (SYSEX_START, ONCE, matchEquals(START_TAG)),
+          (MFR_ID, ONCE, matchEquals(YAMAHA_MFR_ID)),
+          (DEVICE_NUM, ONCE, matchLike("0001nnnn")),
+          (MODEL_ID, ONCE, matchOneOf(matchEquals(SYSTEM1_MODEL_ID), matchEquals(SYSTEM2_MODEL_ID))),
+          (ADDR_HIGH, ONCE, matchSeven),
+          (ADDR_MID, ONCE, matchSeven),
+          (ADDR_LOW, ONCE, matchSeven),
+          (DATA, MANY, matchSeven),
+          (SYSEX_END, ONCE, matchEquals(END_TAG))
+        )
+      ),
+      FrameTable(
+        DX_PARAM_CHANGE, 7,
+        Seq(
+          (SYSEX_START, ONCE, matchEquals(START_TAG)),
+          (MFR_ID, ONCE, matchEquals(YAMAHA_MFR_ID)),
+          (DEVICE_NUM, ONCE, matchLike("0001nnnn")),
+          (PARAMETER_GROUP_NUM, ONCE, matchSeven),
+          (PARAMETER_NUM, ONCE, matchSeven),
+          (DATA, ONCE, matchSeven),
           (SYSEX_END, ONCE, matchEquals(END_TAG))
         )
       )
