43da48a109740589a6b1e9603b812465685ce6a1
Eric Conlon <ejconlon@gmail.com>
2013-12-29 20:01:23 -0500

    rhythm step seq

--- DXEdit.scala
+++ DXEdit.scala
@@ -221,7 +221,7 @@ object DXEdit {
       DataTable(
         VOICE_COMMON_1, 0x29,
         Seq(
-          ("Distortion: Off/On", ONE, Discrete(Set(0x00, 0x01))),
+          ("Distortion: Off/On", ONE, onOff),
           ("Distortion: Drive", ONE, Interval(0x00, 0x64)),
           ("Distortion: AMP Type", ONE, Interval(0x00, 0x03)),
           ("Distortion: LPF Cutoff", ONE, Interval(0x22, 0x3C)),
@@ -232,77 +232,77 @@ object DXEdit {
           ("2-Band EQ Mid Freq", ONE, Interval(0x0E, 0x36)),
           ("2-Band EQ Mid Gain", ONE, Interval(0x34, 0x4C)),
           ("2-Band EQ Mid Resonance(Q)", ONE, Interval(0x0A, 0x78)),
-          ("RESERVED 1", ONE, Interval(0x00, 0x7F)),
-          ("Filter Cutoff", ONE, Interval(0x00, 0x7F)),
+          ("RESERVED 1", ONE, anySeven),
+          ("Filter Cutoff", ONE, anySeven),
           ("Filter Resonance(Q)", ONE, Interval(0x00, 0x74)),
           ("Filter Type", ONE, Interval(0x00, 0x05)),
-          ("Filter Cutoff Scaling Depth", ONE, Interval(0x00, 0x7F)),
+          ("Filter Cutoff Scaling Depth", ONE, anySeven),
           ("Filter Cutoff Modulation Depth", ONE, Interval(0x00, 0x63)),
           ("Filter Input Gain", ONE, Interval(0x34, 0x4C)),
-          ("FEG Attack", ONE, Interval(0x00, 0x7F)),
-          ("FEG Decay", ONE, Interval(0x00, 0x7F)),
-          ("FEG Sustain", ONE, Interval(0x00, 0x7F)),
-          ("FEG Release", ONE, Interval(0x00, 0x7F)),
-          ("FEG Depth", ONE, Interval(0x00, 0x7F)),
-          ("FEG Depth Velocity Sense", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 2", ONE, Interval(0x00, 0x7F)),
+          ("FEG Attack", ONE, anySeven),
+          ("FEG Decay", ONE, anySeven),
+          ("FEG Sustain", ONE, anySeven),
+          ("FEG Release", ONE, anySeven),
+          ("FEG Depth", ONE, anySeven),
+          ("FEG Depth Velocity Sense", ONE, anySeven),
+          ("RESERVED 2", ONE, anySeven),
           ("Noise OSC Type", ONE, Interval(0x00, 0x0F)),
-          ("Mixer Voice Level", ONE, Interval(0x00, 0x7F)),
-          ("Mixer Noise Level", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 1 Harmonic", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 2 Harmonic", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 3 Harmonic", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 1 FM Depth", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 2 FM Depth", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 3 FM Depth", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 1 EG Decay", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 2 EG Decay", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 3 EG Decay", ONE, Interval(0x00, 0x7F)),
-          ("AEG Attack", ONE, Interval(0x00, 0x7F)),
-          ("AEG Decay", ONE, Interval(0x00, 0x7F)),
-          ("AEG Sustain", ONE, Interval(0x00, 0x7F)),
-          ("AEG Release", ONE, Interval(0x00, 0x7F))
+          ("Mixer Voice Level", ONE, anySeven),
+          ("Mixer Noise Level", ONE, anySeven),
+          ("Modulator 1 Harmonic", ONE, anySeven),
+          ("Modulator 2 Harmonic", ONE, anySeven),
+          ("Modulator 3 Harmonic", ONE, anySeven),
+          ("Modulator 1 FM Depth", ONE, anySeven),
+          ("Modulator 2 FM Depth", ONE, anySeven),
+          ("Modulator 3 FM Depth", ONE, anySeven),
+          ("Modulator 1 EG Decay", ONE, anySeven),
+          ("Modulator 2 EG Decay", ONE, anySeven),
+          ("Modulator 3 EG Decay", ONE, anySeven),
+          ("AEG Attack", ONE, anySeven),
+          ("AEG Decay", ONE, anySeven),
+          ("AEG Sustain", ONE, anySeven),
+          ("AEG Release", ONE, anySeven)
         )
       ),
       DataTable(VOICE_COMMON_2, 0x05,
         Seq(
           ("Modulator Select", ONE, Interval(0x00, 0x03)),
-          ("Scene Control", ONE, Interval(0x00, 0x7F)),
+          ("Scene Control", ONE, anySeven),
           ("Common Tempo", MSB, Interval(0x00, 0x4A)),
-          ("Common Tempo", LSB, Interval(0x00, 0x7F)),
+          ("Common Tempo", LSB, anySeven),
           ("Play Effect Swing", ONE, Interval(0x32, 0x53))
         )
       ),
       DataTable(VOICE_SCENE, 0x1C,
         Seq(
-          ("Filter Cutoff", ONE, Interval(0x00, 0x7F)),
+          ("Filter Cutoff", ONE, anySeven),
           ("Filter Resonance(Q)", ONE, Interval(0x00, 0x74)),
-          ("FEG Attack", ONE, Interval(0x00, 0x7F)),
-          ("FEG Decay", ONE, Interval(0x00, 0x7F)),
-          ("FEG Sustain", ONE, Interval(0x00, 0x7F)),
-          ("FEG Release", ONE, Interval(0x00, 0x7F)),
-          ("FEG Depth", ONE, Interval(0x00, 0x7F)),
+          ("FEG Attack", ONE, anySeven),
+          ("FEG Decay", ONE, anySeven),
+          ("FEG Sustain", ONE, anySeven),
+          ("FEG Release", ONE, anySeven),
+          ("FEG Depth", ONE, anySeven),
           ("Filter Type", ONE, Interval(0x00, 0x05)),
           ("LFO Speed", ONE, Interval(0x00, 0x63)),
           ("Portamento Time", ONE, Interval(0x00, 0x63)),
-          ("Mixer Noise Level", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 1 Harmonic", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 2 Harmonic", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 3 Harmonic", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 1 FM Depth", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 2 FM Depth", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 3 FM Depth", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 1 EG Decay", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 2 EG Decay", ONE, Interval(0x00, 0x7F)),
-          ("Modulator 3 EG Decay", ONE, Interval(0x00, 0x7F)),
-          ("AEG Attack", ONE, Interval(0x00, 0x7F)),
-          ("AEG Decay", ONE, Interval(0x00, 0x7F)),
-          ("AEG Sustain", ONE, Interval(0x00, 0x7F)),
-          ("AEG Release", ONE, Interval(0x00, 0x7F)),
-          ("Volume", ONE, Interval(0x00, 0x7F)),
-          ("Pan", ONE, Interval(0x00, 0x7F)),
-          ("Effect Send", ONE, Interval(0x00, 0x7F)),
-          ("Effect Parameter", ONE, Interval(0x00, 0x7F))
+          ("Mixer Noise Level", ONE, anySeven),
+          ("Modulator 1 Harmonic", ONE, anySeven),
+          ("Modulator 2 Harmonic", ONE, anySeven),
+          ("Modulator 3 Harmonic", ONE, anySeven),
+          ("Modulator 1 FM Depth", ONE, anySeven),
+          ("Modulator 2 FM Depth", ONE, anySeven),
+          ("Modulator 3 FM Depth", ONE, anySeven),
+          ("Modulator 1 EG Decay", ONE, anySeven),
+          ("Modulator 2 EG Decay", ONE, anySeven),
+          ("Modulator 3 EG Decay", ONE, anySeven),
+          ("AEG Attack", ONE, anySeven),
+          ("AEG Decay", ONE, anySeven),
+          ("AEG Sustain", ONE, anySeven),
+          ("AEG Release", ONE, anySeven),
+          ("Volume", ONE, anySeven),
+          ("Pan", ONE, anySeven),
+          ("Effect Send", ONE, anySeven),
+          ("Effect Parameter", ONE, anySeven)
         )
       ),
       DataTable(VOICE_FREE_EG, 0x60C,
@@ -310,51 +310,67 @@ object DXEdit {
           ("Free EG Trigger", ONE, Interval(0x00, 0x03)),
           ("Free EG Loop Type", ONE, Interval(0x00, 0x04)),
           ("Free EG Length", ONE, Interval(0x02, 0x60)),
-          ("Free EG Keyboard Track", ONE, Interval(0x00, 0x7F))
+          ("Free EG Keyboard Track", ONE, anySeven)
         ) ++ trackParams ++ trackDatas
       ),
       DataTable(VOICE_STEP_SEQ, 0x66,
         Seq(
           ("Step Seq Base Unit", ONE, Discrete(Set(0x04, 0x06, 0x07))),
           ("Step Seq Length", ONE, Discrete(Set(0x08, 0x0C, 0x10))),
-          ("RESERVED 1", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 2", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 3", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 4", ONE, Interval(0x00, 0x7F))
+          ("RESERVED 1", ONE, anySeven),
+          ("RESERVED 2", ONE, anySeven),
+          ("RESERVED 3", ONE, anySeven),
+          ("RESERVED 4", ONE, anySeven)
         ) ++
-        sixteen("Step Seq Note", ONE, Interval(0x00, 0xF7)) ++
-        sixteen("Step Seq Velocity", ONE, Interval(0x00, 0xF7)) ++
-        sixteen("Step Seq Gate Time", LSB, Interval(0x00, 0xF7)) ++
-        sixteen("Step Seq Control Change", ONE, Interval(0x00, 0xF7)) ++
-        sixteen("Step Seq Gate Time", MSB, Interval(0x00, 0xF7)) ++
-        sixteen("Step Seq Mute", ONE, Discrete(Set(0x00, 0x01)))
+        sixteen("Step Seq Note", ONE, anySeven) ++
+        sixteen("Step Seq Velocity", ONE, anySeven) ++
+        sixteen("Step Seq Gate Time", LSB, anySeven) ++
+        sixteen("Step Seq Control Change", ONE, anySeven) ++
+        sixteen("Step Seq Gate Time", MSB, anySeven) ++
+        sixteen("Step Seq Mute", ONE, onOff)
       ),
       DataTable(EFFECT, 0x03,
         Seq(
           // See effect lookup table
           ("Effect Type", MSB, Discrete(Set(0x00, 0x01, 0x02, 0x03))),
           ("Effect Type", LSB, Discrete(Set(0x00, 0x01, 0x02, 0x03))),
-          ("Effect Parameter", ONE, Interval(0x00, 0x7F))
+          ("Effect Parameter", ONE, anySeven)
         )
       ),
       DataTable(PART_MIX, 0x0F,
         Seq(
-          ("RESERVED 1", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 2", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 3", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 4", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 5", ONE, Interval(0x00, 0x7F)),
-          ("Volume", ONE, Interval(0x00, 0x7F)),
-          ("Pan", ONE, Interval(0x00, 0x7F)),
-          ("Effect 1 Send", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 6", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 7", ONE, Interval(0x00, 0x7F)),
-          ("Filter Cutoff Frequency", ONE, Interval(0x00, 0x7F)),
-          ("Filter Resonance", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 8", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 9", ONE, Interval(0x00, 0x7F)),
-          ("RESERVED 10", ONE, Interval(0x00, 0x7F))
+          reserved(1),
+          reserved(2),
+          reserved(3),
+          reserved(4),
+          reserved(5),
+          ("Volume", ONE, anySeven),
+          ("Pan", ONE, anySeven),
+          ("Effect 1 Send", ONE, anySeven),
+          reserved(6),
+          reserved(7),
+          ("Filter Cutoff Frequency", ONE, anySeven),
+          ("Filter Resonance", ONE, anySeven),
+          reserved(8),
+          reserved(9),
+          reserved(10)
         )
+      ),
+      DataTable(RHYTHM_STEP_SEQ, 0x66,
+        Seq(
+          reserved(1),
+          reserved(2),
+          reserved(3),
+          reserved(4),
+          reserved(5),
+          reserved(6)
+        ) ++
+        sixteen("Step Seq Instrument", ONE, Interval(0x00, 0x78)) ++
+        sixteen("Step Seq Velocity", ONE, anySeven) ++
+        sixteen("Step Seq Gate Time", LSB, anySeven) ++
+        sixteen("Step Seq Pitch", ONE, anySeven) ++
+        sixteen("Step Seq Gate Time", MSB, Interval(0x00, 0x07)) ++
+        sixteen("Step Seq Mute", ONE, onOff)
       )
     )
 
@@ -364,7 +380,7 @@ object DXEdit {
       } yield {
         Seq(
           ("Free EG Track Param "+i, ONE, Interval(0x00, 0x1F)),
-          ("Free EG Track Scene Switch "+i, ONE, Discrete(Set(0x00, 0x01)))
+          ("Free EG Track Scene Switch "+i, ONE, onOff)
         )
       }).flatten
 
@@ -376,8 +392,8 @@ object DXEdit {
           j <- 1 until (192 + 1)
         } yield {
           Seq(
-            ("Free EG Track "+i+" Data "+j, MSB, Interval(0x00, 0x01)),
-            ("Free EG Track "+i+" Data "+j, LSB, Interval(0x00, 0x7F))
+            ("Free EG Track "+i+" Data "+j, MSB, topBit),
+            ("Free EG Track "+i+" Data "+j, LSB, anySeven)
           )
         }).flatten
       }).flatten
@@ -388,6 +404,13 @@ object DXEdit {
       } yield {
         (name+" "+i, ONE, range)
       }
+
+    private[this] lazy val onOff: ByteRange = Discrete(Set(0x00, 0x01))
+    private[this] lazy val topBit: ByteRange = onOff
+    private[this] lazy val anySeven: ByteRange = Interval(0x00, 0x7F)
+
+    private[this] def reserved(i: Int): DataRow =
+      ("RESERVED "+i, ONE, anySeven)
   }
 
   case class Address(modelId: Byte, high: Byte, mid: Byte, low: Byte)
