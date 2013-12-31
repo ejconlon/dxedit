2c0e7103bfcc20d2260c9cbd38205d826f6146e9
Eric Conlon <ejconlon@gmail.com>
2013-12-31 15:00:17 -0800

    extract frames

--- src/main/scala/net/exathunk/dxedit/DXEdit.scala
+++ src/main/scala/net/exathunk/dxedit/DXEdit.scala
@@ -1,13 +1,46 @@
 package net.exathunk.dxedit
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 object DXEdit {
 
   import FrameType._
   import SubFrameType._
 
-  def extractFrames(bytes: Seq[Byte]): Try[Seq[Frame]] = throw TodoException
+  sealed trait ExtractException extends Exception
+  case object MismatchException extends ExtractException
+  case object OverlapException extends ExtractException
+  case object ContiguityException extends ExtractException
+
+  def extractFrames(bytes: Seq[Byte]): Try[Seq[Frame]] = {
+    val starts = bytes.zipWithIndex.collect { case(Constants.START_TAG, i) => i }
+    val ends = bytes.zipWithIndex.collect { case(Constants.END_TAG, i) => i }
+    if (starts.size != ends.size) {
+      Failure(MismatchException)
+    } else {
+      val ivals = starts.zip(ends)
+      val b = Seq.newBuilder[Frame]
+      var exc: Option[ExtractException] = None
+      var lastIndex = -1
+      ivals.foreach { ival =>
+        if (ival._1 > ival._2) {
+          exc = Some(OverlapException)
+        } else if (ival._1 != lastIndex + 1) {
+          exc = Some(ContiguityException)
+        } else {
+          b += bytes.slice(ival._1, ival._2 + 1)
+          lastIndex = ival._2
+        }
+      }
+      if (exc.isDefined) {
+        Failure(exc.get)
+      } else if (lastIndex != bytes.size - 1) {
+        Failure(ContiguityException)
+      } else {
+        Success(b.result)
+      }
+    }
+  }
 
   trait FrameReader {
     def readPSeq(frame: Frame): Try[PSeq]
--- src/test/scala/net/exathunk/dxedit/DXEditTest.scala
+++ src/test/scala/net/exathunk/dxedit/DXEditTest.scala
@@ -1,6 +1,7 @@
 package net.exathunk.dxedit
 
 import org.scalatest.FunSuite
+import scala.util.{Failure, Success}
 
 class DXEditTest extends FunSuite {
 
@@ -61,5 +62,29 @@ class DXEditTest extends FunSuite {
     assert(Some(0xF) == m(0xF))
     assert(Some(0x0) == m(0x0))
     assert(None == m(0x9))
+  }
+
+  val dx200NativeBulkDumpSeq: Seq[Byte] = Seq(
+    0xF0, 0x43, 0x00, 0x62, 0x00, 0x05, 0x21, 0x7F,
+    0x00, 0x03, 0x00, 0x01, 0x0C, 0x32, 0x19, 0xF7)
+
+  test("extractFrames") {
+    // Extract multiple contiguous frames
+    val twoFrames = dx200NativeBulkDumpSeq ++ dx200NativeBulkDumpSeq
+    assert(Success(Seq(dx200NativeBulkDumpSeq, dx200NativeBulkDumpSeq)) == DXEdit.extractFrames(twoFrames))
+
+    // Fail to extract incomplete frame
+    val incomplete = dx200NativeBulkDumpSeq.slice(0, dx200NativeBulkDumpSeq.size - 1)
+    assert(Failure(DXEdit.MismatchException) == DXEdit.extractFrames(incomplete))
+
+    // Fail to extract non-contiguous frames
+    val nonContig = dx200NativeBulkDumpSeq ++ Seq[Byte](0xAB) ++ dx200NativeBulkDumpSeq
+    assert(Failure(DXEdit.ContiguityException) == DXEdit.extractFrames(nonContig))
+
+    val leading = Seq[Byte](0xAB) ++ dx200NativeBulkDumpSeq
+    assert(Failure(DXEdit.ContiguityException) == DXEdit.extractFrames(leading))
+
+    val trailing = dx200NativeBulkDumpSeq ++ Seq[Byte](0xAB)
+    assert(Failure(DXEdit.ContiguityException) == DXEdit.extractFrames(trailing))
   }
 }
